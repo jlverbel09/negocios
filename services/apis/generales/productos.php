b59a0ea5deeb836efe7a107dda6ec9f5307b0597
Jorge Verbel Diaz <jlverbel09@gmail.com>
2026-01-14 22:18:03 +0100

    validacion de estados

--- services/apis/generales/productos.php
+++ services/apis/generales/productos.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
 if (isset($_GET['accion']) && $_GET['accion'] == 'listProductos') {
-    $query = "SELECT * from producto where id_negocio = " . $_GET['id_negocio'];
+    $query = "SELECT * from producto where id_negocio = " . $_GET['id_negocio'] . " and estado = 'A'";
     $data = $conexion->query($query)->fetchAll();
 }
 
