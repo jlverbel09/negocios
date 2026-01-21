0c76a2acca3e66fd877e29b522bac67f1ce6c8c9
Jorge Verbel Diaz <jlverbel09@gmail.com>
2026-01-21 23:18:58 +0100

    ajustes de contador de visitas

--- services/apis/generales/negocio.php
+++ services/apis/generales/negocio.php
@@ -1,6 +1,8 @@
 <?php
 require_once('../../../db/conexion.php');
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 
 if (isset($_GET['accion']) && $_GET['accion'] == 'detalleNegocio') {
 
