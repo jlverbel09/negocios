5cc1c98e41fcc5f2a72803fdfcd5812028b1738b
Jorge Verbel Diaz <jlverbel09@gmail.com>
2026-02-09 17:51:13 +0100

    ajustes de estilo login responsive

--- static/login.php
+++ static/login.php
@@ -8,6 +8,7 @@ if (!empty($_SESSION['usuario'])) {
 <html lang="es">
 
 <head>
+    <title>Georking Business - Iniciar Sesión</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
